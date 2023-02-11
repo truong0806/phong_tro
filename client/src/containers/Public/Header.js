e8214dd233cf4f45b86bf6027cc52a16ae257ab3
Nguyễn Thanh Trường <thanhtruong090123@gmail.com>
2023-02-11 13:19:16 +0000

    fix icon

--- client/src/containers/Public/Header.js
+++ client/src/containers/Public/Header.js
@@ -1,6 +1,12 @@
 import React from "react";
 import logo from "../../assets/logoWithoutBg.png";
+import fontawesome from '@fortawesome/fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 import { Button } from "../../components";
+
+fontawesome.library.add("fa-solid fa-heart");
+
 const Header = () => {
   return (
     <div className="w-1100 flex items-center justify-between bg-red-200">
@@ -9,8 +15,8 @@ const Header = () => {
         src={logo}
         alt="logo"
       />
+      <FontAwesomeIcon icon="fa-solid fa-heart" />
       <div className="flex items-center gap-1">
-        <small className=""></small>
         <Button
           text={"Đăng nhập"}
           textColor="text-white"
--- client/src/containers/Public/Home.js
+++ client/src/containers/Public/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "./Header";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const Home = () => {
   return (
     <div className="w-1100 h-full m-auto border border-red-500">
       <Header />
-    </div>  
+      
+    </div>
   );
 };
 
