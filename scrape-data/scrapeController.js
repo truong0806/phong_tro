c5e4b7233ce85bcbbf219ebd1367a1998b85f9a6
thanhtrieunguyen <nthtrieu204@gmail.com>
2023-02-16 14:28:27 +0700

    add

--- scrape-data/scrapeController.js
+++ scrape-data/scrapeController.js
@@ -2,10 +2,15 @@ const scrapers = require("./scraper");
 
 const scrapeController = async (browserInstance) => {
   const url = "https://phongtro123.com";
+  const indexs = [1, 2, 3, 4];
   try {
     let browser = await browserInstance;
-    //let categories = scrapers.scrapeCategory(browser, url);
-    let city = scrapers.fillter(browser, url);
+    const categories = await scrapers.scrapeCategory(browser, url);
+    const scrapeData = {};
+    const selectedCategories = categories.filter((category, index) =>
+      indexs.some((i) => i === index)
+    );
+    await scrapers.scraper(browser, selectedCategories[0].link);
   } catch (error) {
     console.log("Error in controller: " + error);
   }
--- scrape-data/scraper.js
+++ scrape-data/scraper.js
@@ -22,50 +22,56 @@ const scrapeCategory = (browser, url) =>
           return dataCategories;
         }
       );
-      console.log("Write categories to file...");
-      const data = JSON.stringify(dataCategories);
-      await page.close();
-      fs.writeFile("dataCategoryies.json", data, function (err) {
-        if (err) {
-          console.log(err);
-        }
-      });
-      resolve(page);
+      //console.log("Write categories to file...");
+      // const data = JSON.stringify(dataCategories);
+      // await page.close();
+      // fs.writeFile("dataCategoryies.json", data, function (err) {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      // });
+      resolve(dataCategories);
     } catch (error) {
       console.log("Error in scaper.js: " + error);
       reject(error);
     }
   });
-const fillter = (browser, url) =>
+
+const scraper = (browser, url) =>
   new Promise(async (resolve, reject) => {
     try {
-      let page = await browser.newPage();
+      let newPage = await browser.newPage();
       console.log("Opening browser... ");
-      await page.goto(url);
-      console.log("Opening: " + url);
-      await page.waitForSelector("#webpage");
+      await newPage.goto(url);
+      console.log("Opening url: " + url);
+      await newPage.waitForSelector("#main");
       console.log("Loading complete website...");
 
-      const dataCity = await page.$$eval(
-        ".filter-popup > div filter-popup-content > div > ul  ",
+      const scrapeData = {};
+
+      //Header Data
+      const headerData = await newPage.$eval("header", (element) => {
+        return {
+          title: element.querySelector("h1").innerText,
+          discription: element.querySelector("p").innerText,
+        };
+      });
+      scrapeData.header = headerData;
+      //list link items Data
+      const detailLinks = await newPage.$$eval(
+        "#left-col > section.section-post-listing > ul > li ",
         (elements) => {
-          dataCity = elements.map((Element) => {
-            return {
-              city: Element.querySelector("li").innerText,
-            };
+          detailLinks = elements.map((element) => {
+            return element.querySelector(".post-meta > h3 > a").href;
           });
-          return dataCity;
+          return detailLinks;
         }
       );
-      console.log("Write city to file...");
-      const data = JSON.stringify(dataCity);
-      await page.close();
-      fs.writeFile("city.json", data, function (err) {
-        if (err) {
-          console.log(err);
-        }
-      });
-      resolve(page);
+      console.log(detailLinks);
+
+      await browser.close();
+      console.log("Closed browser...");
+      resolve();
     } catch (error) {
       console.log("Error in scaper.js: " + error);
       reject(error);
@@ -73,5 +79,5 @@ const fillter = (browser, url) =>
   });
 module.exports = {
   scrapeCategory,
-  fillter,
+  scraper,
 };
